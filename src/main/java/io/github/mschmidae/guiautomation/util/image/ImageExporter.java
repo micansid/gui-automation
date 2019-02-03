d91d0a15ea13c780b4caef4d38ad9fc7435f908e
Michael Schmid <m.schmid@hm.edu>
2019-02-03 21:29:41 +0100

    ImageExporter

--- src/main/java/io/github/mschmidae/guiautomation/util/image/ImageExporter.java
+++ src/main/java/io/github/mschmidae/guiautomation/util/image/ImageExporter.java
@@ -1,4 +1,24 @@
 package io.github.mschmidae.guiautomation.util.image;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class ImageExporter {
+    public boolean export(final BufferedImage image, final String path) {
+        boolean result = false;
+        try {
+            File outputfile = new File(path + ".png");
+            ImageIO.write(image, "png", outputfile);
+            result = true;
+        } catch (IOException e) {
+            //result ist false
+        }
+        return result;
+    }
+
+    public boolean export(final Image image, final String path) {
+        return export(image.bufferedImage(), path);
+    }
 }
