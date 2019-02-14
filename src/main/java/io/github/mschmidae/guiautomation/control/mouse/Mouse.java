5f3da6fd626b2397d2f4b07431b0a6604afa1d82
Michael Schmid <m.schmid@hm.edu>
2019-02-14 10:54:42 +0100

    ImagePatternRecorder

--- src/main/java/io/github/mschmidae/guiautomation/control/mouse/Mouse.java
+++ src/main/java/io/github/mschmidae/guiautomation/control/mouse/Mouse.java
@@ -1,4 +1,23 @@
 package io.github.mschmidae.guiautomation.control.mouse;
 
+import io.github.mschmidae.guiautomation.util.Position;
+import java.util.function.Supplier;
+
 public class Mouse {
+  private final Supplier<Position> positionSupplier;
+
+  public Mouse () {
+    this(new AwtMousePositionSupplier());
+  }
+  public Mouse(final Supplier<Position> positionSupplier) {
+    this.positionSupplier = positionSupplier;
+  }
+
+  public Position currentPosition() {
+    return getPositionSupplier().get();
+  }
+
+  private Supplier<Position> getPositionSupplier() {
+    return positionSupplier;
+  }
 }
