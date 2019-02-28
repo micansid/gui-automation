87556dc1a03a50433ec4e7a7b250672992cdfeca
Michael Schmid <schmid71@hm.edu>
2019-02-28 21:41:59 +0100

    Windows 10 integration tests with controller

--- src/test/java/io/github/mschmidae/guiautomation/run/Windows10IntegrationTest.java
+++ src/test/java/io/github/mschmidae/guiautomation/run/Windows10IntegrationTest.java
@@ -1,8 +1,7 @@
 package io.github.mschmidae.guiautomation.run;
 
-import io.github.mschmidae.guiautomation.control.clipboard.Clipboard;
+import io.github.mschmidae.guiautomation.control.Controller;
 import io.github.mschmidae.guiautomation.control.keyboard.Key;
-import io.github.mschmidae.guiautomation.control.keyboard.Keyboard;
 import io.github.mschmidae.guiautomation.control.keyboard.Shortcut;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -16,47 +15,45 @@ class Windows10IntegrationTest {
 
   @Test
   void openNotepadTypeTextCopyToClipboard() throws InterruptedException {
-    Keyboard keyboard = new Keyboard();
-    Clipboard clipboard = new Clipboard();
+    Controller controller = new Controller();
 
-    keyboard.press(Key.WINDOWS).input(Key.R).release(Key.WINDOWS);
+    controller.keyboard().press(Key.WINDOWS).input(Key.R).release(Key.WINDOWS);
     Thread.sleep(SLEEP_TIME);
-    keyboard.type("notepad").input(Key.ENTER);
+    controller.keyboard().type("notepad").input(Key.ENTER);
 
     Thread.sleep(SLEEP_TIME);
-    keyboard.press(Key.SHIFT).input(Key.NUM_8).release(Key.SHIFT)
+    controller.keyboard().press(Key.SHIFT).input(Key.NUM_8).release(Key.SHIFT)
         .type("aAbB")
         .press(Key.SHIFT).input(Key.NUM_9).release(Key.SHIFT);
 
     Thread.sleep(SLEEP_TIME);
-    keyboard.execute(Shortcut.MARK_ALL).execute(Shortcut.COPY);
+    controller.keyboard().execute(Shortcut.MARK_ALL).execute(Shortcut.COPY);
     Thread.sleep(SLEEP_TIME);
-    keyboard.execute(Shortcut.CLOSE).input(Key.TAB).input(Key.ENTER);
+    controller.keyboard().execute(Shortcut.CLOSE).input(Key.TAB).input(Key.ENTER);
 
-    assertThat(clipboard.get()).isPresent().contains("(aAbB)");
+    assertThat(controller.clipboard().get()).isPresent().contains("(aAbB)");
   }
 
   @Test
   void openNotepadInsertFromClipboardModifyTextCopyToClipboard() throws InterruptedException {
-    Keyboard keyboard = new Keyboard();
-    Clipboard clipboard = new Clipboard();
+    Controller controller = new Controller();
 
-    keyboard.press(Key.WINDOWS).input(Key.R).release(Key.WINDOWS);
+    controller.keyboard().press(Key.WINDOWS).input(Key.R).release(Key.WINDOWS);
     Thread.sleep(SLEEP_TIME);
-    keyboard.type("notepad").input(Key.ENTER);
-
-    Thread.sleep(SLEEP_TIME);
-    clipboard.set("TEST");
-    keyboard.execute(Shortcut.PASTE);
+    controller.keyboard().type("notepad").input(Key.ENTER);
 
     Thread.sleep(SLEEP_TIME);
-    keyboard.input(Key.HOME).press(Key.SHIFT).input(Key.NUM_8).release(Key.SHIFT);
-    keyboard.input(Key.END).press(Key.SHIFT).input(Key.NUM_9).release(Key.SHIFT);
+    controller.clipboard().set("TEST");
+    controller.keyboard().execute(Shortcut.PASTE);
 
     Thread.sleep(SLEEP_TIME);
-    keyboard.execute(Shortcut.MARK_ALL).execute(Shortcut.COPY);
+    controller.keyboard().input(Key.HOME).press(Key.SHIFT).input(Key.NUM_8).release(Key.SHIFT);
+    controller.keyboard().input(Key.END).press(Key.SHIFT).input(Key.NUM_9).release(Key.SHIFT);
+
     Thread.sleep(SLEEP_TIME);
-    keyboard.execute(Shortcut.CLOSE).input(Key.TAB).input(Key.ENTER);
-    assertThat(clipboard.get()).isPresent().contains("(TEST)");
+    controller.keyboard().execute(Shortcut.MARK_ALL).execute(Shortcut.COPY);
+    Thread.sleep(SLEEP_TIME);
+    controller.keyboard().execute(Shortcut.CLOSE).input(Key.TAB).input(Key.ENTER);
+    assertThat(controller.clipboard().get()).isPresent().contains("(TEST)");
   }
 }
